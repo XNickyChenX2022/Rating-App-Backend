efc36db577a9e6f7ae317f1c0932c5cfbcb053e3
Nicky Chen <nickychen2022@gmail.com>
2023-07-19 12:57:07 -0400

    updated typo

--- controllers/webhookController.js
+++ controllers/webhookController.js
@@ -1,30 +1,27 @@
 import asyncHandler from "express-async-handler";
 
 const registerWebhook = asyncHandler(async (req, res) => {
-    console.log(process.env.WEBHOOK_URL + "/api/webhook/create");
-    try {
-      const response = await fetch("https://api.igdb.com/v4/games/webhooks/", {
-        method: "POST",
-        headers: {
-          Accept: "application/x-www-form-urlencoded",
-          "Client-ID": process.env.CLIENT_ID,
-          Authorization: "Bearer " + process.env.access_token,
-        },
-        body: new URLSearchParams({
-          url: process.env.WEBHOOK_URL + "/api/webhook/create",
-          secret: process.env.WEBHOOK_SECRET,
-          method: "create",
-        }).toString(),
-      });
-      const data = await response.json();
-      // console.log(data);
-      res.status(200).send(data);
-    } catch (error) {
-      res.status(400);
-      throw new Error("unable to register webhook");
-    }
-
-
+  try {
+    const response = await fetch("https://api.igdb.com/v4/games/webhooks/", {
+      method: "POST",
+      headers: {
+        Accept: "application/x-www-form-urlencoded",
+        "Client-ID": process.env.CLIENT_ID,
+        Authorization: "Bearer " + process.env.access_token,
+      },
+      body: new URLSearchParams({
+        url: process.env.WEBHOOK_URL + "/api/webhooks/create",
+        secret: process.env.WEBHOOK_SECRET,
+        method: "create",
+      }).toString(),
+    });
+    const data = await response.json();
+    // console.log(data);
+    res.status(200).send(data);
+  } catch (error) {
+    res.status(400);
+    throw new Error("unable to register webhook");
+  }
 });
 
 const createGames = asyncHandler(async (req, res) => {
