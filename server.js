e4876a99bf8572b08f14d40a8ab73e7632454243
Nicky Chen <nickychen2022@gmail.com>
2023-08-02 14:42:08 -0400

    modified path for frontend url

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: process.env.frontend_url,
+    origin: process.env.FRONTEND_URL,
     credentials: true,
   })
 );
