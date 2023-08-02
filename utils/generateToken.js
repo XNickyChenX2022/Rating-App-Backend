edbdcf087143212725eebd396629a4b70ad42d22
Nicky Chen <nickychen2022@gmail.com>
2023-08-02 19:15:19 -0400

    fixing issue with cookies not working

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -11,7 +11,6 @@ const generateToken = (res, userId) => {
     sameSite: "strict",
     maxAge: 30 * 24 * 60 * 60 * 1000,
   });
-  console.log("cookie sent to user");
 };
 
 export default generateToken;
