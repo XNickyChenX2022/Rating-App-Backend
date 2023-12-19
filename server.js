d591032ec2c8e505b192f05c1a2e4f4cd9cab406
Nicky Chen <115733348+XNickyChenX2022@users.noreply.github.com>
2023-12-19 16:35:16 -0500

    Disabled Redis for now

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ import gameRoutes from "./routes/gameRoutes.js";
 import cookieParser from "cookie-parser";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import connectDB from "./config/db.js";
-import connectRedis from "./config/client.js";
+// import connectRedis from "./config/client.js";
 import registerWebhooks from "./config/webhook.js";
 import friendRoutes from "./routes/friendRoutes.js";
 import cors from "cors";
@@ -14,7 +14,7 @@ dotenv.config();
 const port = process.env.PORT || 5000;
 
 connectDB();
-connectRedis();
+// connectRedis();
 registerWebhooks();
 const app = express();
 app.use(express.json());
