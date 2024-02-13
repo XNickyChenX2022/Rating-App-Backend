be0ace0147054b6a0200331e64791f3626ea54b7
Nicky Chen <nickychen2022@gmail.com>
2024-02-13 14:54:24 -0500

    readded cors

--- server.js
+++ server.js
@@ -20,12 +20,12 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-// app.use(
-//   cors({
-//     origin: "https://xnickychenx2022.github.io",
-//     credentials: true,
-//   })
-// );
+app.use(
+  cors({
+    origin: "https://xnickychenx2022.github.io",
+    credentials: true,
+  })
+);
 app.use("/api/users", userRoutes);
 // app.use("/api/webhooks", webhookRoutes);
 app.use("/api/games", gameRoutes);
